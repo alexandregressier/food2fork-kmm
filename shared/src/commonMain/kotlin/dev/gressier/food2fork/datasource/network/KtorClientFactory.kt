489358f15c425739e2d3d2f2954b1a40bee38ea8
Alexandre Gressier <alexandre@gressier.dev>
2021-11-11 11:49:10 +0100

    Implement the converter between the Recipe domain & network models

--- shared/src/commonMain/kotlin/dev/gressier/food2fork/datasource/network/KtorClientFactory.kt
+++ shared/src/commonMain/kotlin/dev/gressier/food2fork/datasource/network/KtorClientFactory.kt
@@ -1,7 +1,26 @@
 package dev.gressier.food2fork.datasource.network
 
+import dev.gressier.food2fork.datasource.network.model.RecipeDto
+import dev.gressier.food2fork.domain.model.Recipe
+import dev.gressier.food2fork.domain.util.DateTimeUtil
 import io.ktor.client.*
 
 expect class KtorClientFactory {
     fun build(): HttpClient
 }
+
+fun RecipeDto.toRecipe(): Recipe =
+    Recipe(
+        id = pk,
+        title,
+        publisher,
+        featuredImageUrl = featuredImage,
+        rating,
+        sourceUrl,
+        ingredients,
+        addedAt = DateTimeUtil.toLocalDate(longDateAdded.toDouble()),
+        updatedAt = DateTimeUtil.toLocalDate(longDateUpdated.toDouble()),
+    )
+
+fun List<RecipeDto>.toRecipeList(): List<Recipe> =
+    map(RecipeDto::toRecipe)
---
+++ shared/src/commonMain/kotlin/dev/gressier/food2fork/domain/util/DateTimeUtil.kt
@@ -0,0 +1,37 @@
+package dev.gressier.food2fork.domain.util
+
+import kotlinx.datetime.*
+
+object DateTimeUtil {
+
+    fun now(): LocalDateTime =
+        Clock.System.now().toLocalDateTime(TimeZone.UTC)
+
+    fun toLocalDate(date: Double): LocalDateTime =
+        Instant.fromEpochMilliseconds(date.toLong()).toLocalDateTime(TimeZone.UTC)
+
+    fun toEpochMilliseconds(date: LocalDateTime): Double =
+        date.toInstant(TimeZone.UTC).toEpochMilliseconds().toDouble()
+
+    fun humanizeDatetime(dateTime: LocalDateTime?): String =
+        StringBuilder().also { sb ->
+            dateTime?.run {
+                val hour =
+                    if (hour > 12)
+                        "${hour - 12} PM"
+                    else if (hour != 0)
+                        "$hour AM"
+                    else "midnight"
+
+                val today = now()
+                val tomorrow = Clock.System.now().plus(1, DateTimeUnit.DAY, TimeZone.UTC).toLocalDateTime(TimeZone.UTC)
+                sb.append(
+                    when (date) {
+                        today.date -> "Today at $hour"
+                        tomorrow.date -> "Tomorrow at $hour"
+                        else -> "${date.month.name.lowercase()} ${date.dayOfMonth}"
+                    }
+                )
+            } ?: sb.append("Unknown")
+        }.toString()
+}
