f83f16a767f69b1f1ff993f353bb8f242fbf1935
Alexandre Gressier <alexandre@gressier.dev>
2021-11-11 23:43:33 +0100

    Delete useless blank lines

--- shared/src/commonMain/kotlin/dev/gressier/food2fork/datasource/cache/RecipeCache.kt
+++ shared/src/commonMain/kotlin/dev/gressier/food2fork/datasource/cache/RecipeCache.kt
@@ -4,15 +4,9 @@ import dev.gressier.food2fork.domain.model.Recipe
 import dev.gressier.food2fork.domain.model.RecipeId
 
 interface RecipeCache {
-
     fun insert(recipe: Recipe)
-
     fun insert(recipes: List<Recipe>)
-
     fun getAll(page: Int): List<Recipe>
-
     fun search(query: String, page: Int): List<Recipe>
-
-    @Throws(NullPointerException::class)
-    fun get(recipeId: RecipeId): Recipe?
+    @Throws(NullPointerException::class) fun get(recipeId: RecipeId): Recipe?
 }
