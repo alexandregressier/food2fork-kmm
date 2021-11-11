32c050556f57636376d78b6b2c559a05248464a3
Alexandre Gressier <alexandre@gressier.dev>
2021-11-11 15:56:28 +0100

    Replace vars with vals in the Recipe DTO

--- shared/src/commonMain/kotlin/dev/gressier/food2fork/datasource/network/model/RecipeDto.kt
+++ shared/src/commonMain/kotlin/dev/gressier/food2fork/datasource/network/model/RecipeDto.kt
@@ -5,13 +5,13 @@ import kotlinx.serialization.Serializable
 
 @Serializable
 data class RecipeDto(
-    @SerialName("pk") var pk: Int,
-    @SerialName("title") var title: String,
-    @SerialName("publisher") var publisher: String,
-    @SerialName("featured_image") var featuredImage: String,
-    @SerialName("rating") var rating: Int = 0,
-    @SerialName("source_url") var sourceUrl: String,
-    @SerialName("ingredients") var ingredients: List<String> = emptyList(),
-    @SerialName("long_date_added") var longDateAdded: Long,
-    @SerialName("long_date_updated") var longDateUpdated: Long,
+    @SerialName("pk") val pk: Int,
+    @SerialName("title") val title: String,
+    @SerialName("publisher") val publisher: String,
+    @SerialName("featured_image") val featuredImage: String,
+    @SerialName("rating") val rating: Int = 0,
+    @SerialName("source_url") val sourceUrl: String,
+    @SerialName("ingredients") val ingredients: List<String> = emptyList(),
+    @SerialName("long_date_added") val longDateAdded: Long,
+    @SerialName("long_date_updated") val longDateUpdated: Long,
 )
